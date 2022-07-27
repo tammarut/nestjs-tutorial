3d1c3f93ed993350aea4e82271a9a0127f045a46
Pleum Tammarut <tammarut@mercular.com>
2022-07-27 23:53:28 +0700

    🎨 style: format codes

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,38 +2,38 @@ import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/commo
 import { BooksService } from './books.service';
 
 class CreateBookDTO {
-  readonly id: number
-  readonly title: string
-  readonly description: string
-  readonly author: string
+  readonly id: number;
+  readonly title: string;
+  readonly description: string;
+  readonly author: string;
 }
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly bookService: BooksService) { }
+  constructor(private readonly bookService: BooksService) {}
 
   @Get()
   async getAllBooks() {
-    const books = await this.bookService.getAllBooks()
-    return books
+    const books = await this.bookService.getAllBooks();
+    return books;
   }
 
   @Get(':bookId')
   async getBook(@Param('bookId') bookId: string) {
-    const book = await this.bookService.getBook(bookId)
-    return book
+    const book = await this.bookService.getBook(bookId);
+    return book;
   }
 
   @Post()
   async addNewBook(@Body() createBookDTO: CreateBookDTO) {
-    const books = await this.bookService.addNewBook(createBookDTO)
-    return books
+    const books = await this.bookService.addNewBook(createBookDTO);
+    return books;
   }
 
   @Delete()
   async deleteBook(@Query() query: { bookId: string }) {
-    const { bookId } = query
-    const books = await this.bookService.deleteBook(bookId)
-    return books
+    const { bookId } = query;
+    const books = await this.bookService.deleteBook(bookId);
+    return books;
   }
 }
