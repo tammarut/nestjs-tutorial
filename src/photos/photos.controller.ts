2ce8af65028d3acad79d19179785c31477edd6bf
Pleum Tammarut <tammarut@mercular.com>
2022-07-28 23:54:18 +0700

    ✨ feat(module/photos): upload a file ➝ to `diskStorage` and read file contents

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -1,38 +1,63 @@
-import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
+import fs from 'fs/promises';
+import multer from 'multer';
+import path from 'path';
+import readXlsxFile from 'read-excel-file/node';
 
 @Controller('photos')
 export class PhotoController {
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (_req, file, callback) => {
-          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const fileExtension = extname(file.originalname);
-          const filename = `${file.originalname}_${uniqueSuffix}${fileExtension}`;
-          callback(null, filename);
+      storage: multer.diskStorage({
+        destination: './tmp/sku-mapping',
+        filename: (_, file, callback) => {
+          const fileExtension = path.extname(file.originalname);
+          const filename = path.basename(file.originalname, fileExtension);
+          const randomSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const newFilename = `${filename}_${randomSuffix}${fileExtension}`;
+          callback(null, newFilename);
         },
       }),
+      fileFilter: (_, file, callback) => {
+        const fileExtension = path.extname(file.originalname);
+        if (!fileExtension.includes('.xlsx')) {
+          return callback(new Error('Only Xlsx file are allowed!'), false);
+        }
+        callback(null, true);
+      },
+      // storage: memoryStorage(),
     })
   )
-  uploadSingle(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-    const response = {
-      filename: file.filename,
-      originalname: file.originalname,
-      message: 'Upload successfully',
-    };
-    return response;
+  async uploadSingle(@UploadedFile() file: Express.Multer.File) {
+    try {
+      const rows = await readXlsxFile(file.path);
+      const response = {
+        message: 'Upload successfully',
+        originalname: file.originalname,
+        data: rows,
+      };
+      return response;
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    } finally {
+      fs.unlink(file.path);
+    }
   }
 
-  @Get(':image_path')
-  getFilePath(@Param('image_path') image: string, @Res() response: Response) {
-    console.info(image);
-    return response.sendFile(image, { root: './uploads' });
+  @Get('filename')
+  getFilePath(@Param('filename') filename: string, @Res() response: Response) {
+    return response.sendFile(filename, { root: './tmp/sku-mapping' });
   }
 }
