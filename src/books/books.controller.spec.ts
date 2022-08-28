87112893ba567ceb90149d41aac7f710e47fe3ea
Pleum Tammarut <tammarut@mercular.com>
2022-08-28 18:27:16 +0700

    🧪 test(module/books): DI `BookService` into BookController and clear all mocks before each tests

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,15 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
 
 describe('BooksController', () => {
   let controller: BooksController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
+      providers: [
+        BooksService,
+        {
+          provide: BooksService,
+          useValue: () => ({
+            getAllBooks: jest.fn().mockResolvedValueOnce([]),
+            getBook: jest.fn().mockResolvedValueOnce({}),
+            addNewBook: jest.fn().mockResolvedValueOnce([]),
+            deleteBook: jest.fn().mockResolvedValueOnce([]),
+          }),
+        },
+      ],
     }).compile();
 
     controller = module.get<BooksController>(BooksController);
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   it('should be defined', () => {
