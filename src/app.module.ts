95ce2829cb95c538eb4161ca1e7a1f456aae577f
Pleum Tammarut <tammarut@mercular.com>
2022-10-03 15:26:49 +0700

    ✨ feat: typeorm connect to the MariaDB (MySQL)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { LoggerMiddleware } from './infrastructure/middlewares/logger.middleware';
+import { OrdersModule } from './modules/orders/orders.module';
 import { PhotoModule } from './photos/photos.module';
 
 @Module({
-  imports: [BooksModule, PhotoModule, EventEmitterModule.forRoot()],
+  imports: [
+    BooksModule,
+    PhotoModule,
+    EventEmitterModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'dev-db.vpc.mercular.com',
+      username: 'mercular',
+      password: 'dABZe9Z8Yvs1mQKhRC',
+      database: 'mercular_operation',
+      // entities: [__dirname + '../**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      port: 3306,
+      bigNumberStrings: false,
+      synchronize: false,
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
