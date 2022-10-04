9e7a4af2f2a84965d07feb86b907235d4d46157c
Pleum Tammarut <tammarut@mercular.com>
2022-10-04 21:20:30 +0700

    ♻️ refactor(modules/order): replace try/catch ➝ with radash `_.try`

--- src/modules/orders/use-case/query/ordersQuery.service.ts
+++ src/modules/orders/use-case/query/ordersQuery.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import _ from 'radash';
 import { Repository } from 'typeorm';
 import { OrderEntity } from '../../repository/order.entity';
 
@@ -12,8 +13,8 @@ export class OrdersService {
     private readonly orderRepository: Repository<OrderEntity>
   ) {}
 
-  async findOne(id: number): Promise<OrderEntity | null> {
-    this.logger.log(`Receive orderId: ${id}`);
+  async findOne(orderId: number): Promise<OrderEntity | null> {
+    this.logger.log(`Receive orderId: ${orderId}`);
     // ➢Solution 1: Execute Raw SQL
     //  const queryString = 'SELECT * FROM orders WHERE id = ?';
     //  const order = await this.orderRepository.query(queryString, [id]);
@@ -23,19 +24,33 @@ export class OrdersService {
     // const order = await this.orderRepository.findOneBy({ id: id });
 
     // ———————— Join tables —————————
-    try {
-      const order = await this.orderRepository.findOne({
-        where: { id: id },
-        relations: { orderItems: true },
-      });
-      if (!order) {
-        this.logger.error({ id: 'query-order-error' }, `Not found this orderId (${id})`);
-        throw new Error(`Not found this orderId ${id}`);
-      }
-      return order;
-    } catch (err) {
+    const [err, order] = await _.try(this.orderRepository.findOne)({
+      where: { id: orderId },
+      relations: { orderItems: true },
+    });
+    if (err) {
       this.logger.error(err.message, err.stack);
       throw err;
     }
+
+    if (!order) {
+      this.logger.error({ id: 'query-order-error' }, `Not found this orderId (${orderId})`);
+      throw new Error(`Not found this orderId ${orderId}`);
+    }
+    return order;
+    // try {
+    //   const order = await this.orderRepository.findOne({
+    //     where: { id: orderId },
+    //     relations: { orderItems: true },
+    //   });
+    //   if (!order) {
+    //     this.logger.error({ id: 'query-order-error' }, `Not found this orderId (${orderId})`);
+    //     throw new Error(`Not found this orderId ${orderId}`);
+    //   }
+    //   return order;
+    // } catch (err) {
+    //   this.logger.error(err.message, err.stack);
+    //   throw err;
+    // }
   }
 }
