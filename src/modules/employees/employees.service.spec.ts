8514592014503babd3efca9df16b2cb7793c15f1
Pleum Tammarut <tammarut@mercular.com>
2022-12-13 18:15:59 +0700

    ➕dependency(jest): install a new library @golevelup/ts-jest for mocking

--- src/modules/employees/employees.service.spec.ts
+++ src/modules/employees/employees.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Repository } from 'typeorm/repository/Repository';
+
 import { EmployeesService } from './employees.service';
+import { ContactInfo } from './repository/contact-info.entity';
+import { Employee } from './repository/employee.entity';
+import { Meeting } from './repository/meeting.entity';
+import { Task } from './repository/task.entity';
 
 describe('EmployeesService', () => {
-  let service: EmployeesService;
+  let employeeService: EmployeesService;
+  let employeeRepo: DeepMocked<Repository<Employee>>;
 
   beforeEach(async () => {
+    const mockEmployeeRepo = createMock<Repository<Employee>>();
+    const mockContactInfoRepo = createMock<Repository<ContactInfo>>();
+    const mockTaskRepo = createMock<Repository<Task>>();
+    const mockMeetingRepo = createMock<Repository<Meeting>>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeesService],
+      providers: [
+        EmployeesService,
+        { provide: getRepositoryToken(Employee), useValue: mockEmployeeRepo },
+        { provide: getRepositoryToken(ContactInfo), useValue: mockContactInfoRepo },
+        { provide: getRepositoryToken(Task), useValue: mockTaskRepo },
+        { provide: getRepositoryToken(Meeting), useValue: mockMeetingRepo },
+      ],
     }).compile();
 
-    service = module.get<EmployeesService>(EmployeesService);
+    employeeService = module.get<EmployeesService>(EmployeesService);
+    employeeRepo = module.get(getRepositoryToken(Employee));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(employeeService).toBeDefined();
+    expect(employeeRepo).toBeDefined();
   });
 });
